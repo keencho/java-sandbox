c12382974601afeca1c6f8a006583a440b251bce
keencho <seyoung050412@gmail.com>
2022-05-08 22:35:26 +0900

    setting

---
+++ querydsl/src/main/java/com/sycho/spring/querydsl/QueryDSLConfig.java
@@ -0,0 +1,23 @@
+package com.sycho.spring.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class QueryDSLConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public QueryDSLConfig() {
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
