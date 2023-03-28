c93c2d6c7b80d21ff4311bfc20d59512d8996b23
keencho <seyoung050412@gmail.com>
2023-03-28 14:00:31 +0900

    [algorithm] 정렬, 이진탐색

---
+++ algorithm/src/main/java/com/keencho/algorithm/programmers_코딩테스트_문제풀이전략/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 프로그래머스 코딩테스트 문제 풀이 전략 : 자바 편
+ *
+ * 책 따라가며 공부 / 푼 내용 정리
+ */
+package com.keencho.algorithm.programmers_코딩테스트_문제풀이전략;
---
+++ algorithm/src/main/java/com/keencho/algorithm/programmers_코딩테스트_문제풀이전략/이진탐색/징검다리.java
@@ -0,0 +1,45 @@
+package com.keencho.algorithm.programmers_코딩테스트_문제풀이전략.이진탐색;
+
+import java.util.Arrays;
+
+// p.303
+// https://school.programmers.co.kr/learn/courses/30/lessons/43236
+// 제출안함 --> 문제 이해 안됨;
+public class 징검다리 {
+
+    private void doWork(int distance, int[] rocks, int n) {
+
+    }
+
+    public int solution(int distance, int[] rocks, int n) {
+        rocks = Arrays.copyOf(rocks, rocks.length + 1);
+        rocks[rocks.length - 1] = distance;
+
+        Arrays.sort(rocks);
+
+        var start = 1;
+        var end = distance + 1;
+
+        while (end - start > 1) {
+            var d = (start + end) / 2;
+
+            var removed = 0;
+            var last = 0;
+            for (var rock : rocks) {
+                if (rock - last < d) {
+                    removed++;
+                    continue;
+                }
+                last = rock;
+            }
+
+            if (removed <= n) {
+                start = d;
+            } else {
+                end = d;
+            }
+        }
+
+        return start;
+    }
+}
